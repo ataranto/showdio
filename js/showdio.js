c3a0802e3a0f656f344e1b64b6eb39b5248b918f
anthony taranto <anthony.taranto@rd.io>
2012-11-14 01:25:10 -0800

    make api calls from js

--- js/showdio.js
+++ js/showdio.js
@@ -1,5 +1,7 @@
+var songkick_artists_map = {};
+
 $(document).ready(function() {
-    $.getJSON("http://api.songkick.com/api/3.0/events.json?location=clientip&apikey=G2KCF6q91g23Q6Zh&jsoncallback=?", function(data) {
+    $.getJSON("http://api.songkick.com/api/3.0/events.json?location=clientip&apikey=G2KCF6q91g23Q6Zh&jsoncallback=?", function(response) {
         var template =
             '{{#event}}' +
               '<tr>' +
@@ -9,23 +11,38 @@ $(document).ready(function() {
                 '<td> Buy Tickets </td>' +
               '</tr>' +
             '{{/event}}';
-        var rows = $.mustache(template, data.resultsPage.results);
+        var rows = $.mustache(template, response.resultsPage.results);
         $('#body').append(rows);
 
-        var events = data.resultsPage.results.event;
-        var artists_map = {};
+        var events = response.resultsPage.results.event;
         for (var x = 0; x < events.length; x++) {
             for (var y = 0; y < events[x].performance.length; y++) {
                 var artist = events[x].performance[y].artist.displayName;
-                artists_map[artist] = true;
+                songkick_artists_map[artist] = true;
             }
         }
+    });
+});
 
-        var artists_list = new Array();
-        for (artist in artists_map) {
-            artists_list.push(artist);
-        }
-
-        alert(JSON.stringify(artists_list))
+R.ready(function() {
+    R.authenticate(function() {
+        R.request({
+            method: "getArtistsInCollection",
+            content: {
+                count: 100,
+            },
+            success: function(response) {
+                for (var x = 0; x < response.result.length; x++) {
+                    var artist = response.result[x].name;
+                    //alert(artist);
+                    if (artist in songkick_artists_map) {
+                        alert(artist);
+                    }
+                }
+            },
+            error: function(response) {
+                alert("error");
+            },
+        });
     });
 });
