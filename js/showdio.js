588a160405299918afe5990714976cafd18e865b
anthony taranto <anthony.taranto@rd.io>
2012-11-30 12:43:35 -0800

    showdio.js: check if already authenticated

--- js/showdio.js
+++ js/showdio.js
@@ -1,76 +1,64 @@
-var songkick_artists_map = {};
+(function() {
+    var songkick_artists_map = {};
 
-var event_template =
-  '{{#event}}' +
-    '<tr id="{{ id }}"> ' +
-      '<td>{{ start.date }}</td>' +
-      '<td>' + 
-        '{{#performance}}' +
-          '<p>{{ artist.displayName }}</p>' +
-        '{{/performance}}' +
-      '</td>' + 
-      '<td>{{ venue.displayName }}</td>' +
-      '<td> Buy Tickets </td>' +
-    '<tr>' +
-  '{{/event}}';
-  ;
+    var event_template =
+      '{{#event}}' +
+        '<tr id="{{ id }}"> ' +
+          '<td>{{ start.date }}</td>' +
+          '<td>' + 
+            '{{#performance}}' +
+              '<p>{{ artist.displayName }}</p>' +
+            '{{/performance}}' +
+          '</td>' + 
+          '<td>{{ venue.displayName }}</td>' +
+          '<td> Buy Tickets </td>' +
+        '<tr>' +
+      '{{/event}}';
+      ;
 
-function get_events(page) {
-    if (page > 5) {
-        return;
-    }
-
-    var url =
-        'http://api.songkick.com/api/3.0/events.json' +
-        '?location=clientip' +
-        '&apikey=G2KCF6q91g23Q6Zh' +
-        '&page=' + page +
-        '&jsoncallback=?';
-    $.getJSON(url, function(response) {
-        if (response.resultsPage.status != 'ok') {
+    function get_events(page) {
+        if (page > 5) {
             return;
         }
 
-        var rows = $.mustache(event_template, response.resultsPage.results);
-        $('#tbody').append(rows);
-
-        var event = response.resultsPage.results.event;
-        for (var x = 0; x < event.length; x++) {
-            for (var y = 0; y < event[x].performance.length; y++) {
-                var artist = event[x].performance[y].artist.displayName;
-
-                if (songkick_artists_map[artist] === undefined) {
-                    songkick_artists_map[artist] = [];
-                }
-                songkick_artists_map[artist].push(event[x].id);
+        var url =
+            'http://api.songkick.com/api/3.0/events.json' +
+            '?location=clientip' +
+            '&apikey=G2KCF6q91g23Q6Zh' +
+            '&page=' + page +
+            '&jsoncallback=?';
+        $.getJSON(url, function(response) {
+            if (response.resultsPage.status != 'ok') {
+                return;
             }
-        }
 
-        get_events(response.resultsPage.page + 1);
-    });
-}
+            var rows = $.mustache(event_template, response.resultsPage.results);
+            $('#tbody').append(rows);
 
-$(document).ready(function() {
-    get_events(1);
-});
+            var event = response.resultsPage.results.event;
+            for (var x = 0; x < event.length; x++) {
+                for (var y = 0; y < event[x].performance.length; y++) {
+                    var artist = event[x].performance[y].artist.displayName;
 
-R.ready(function() {
-    R.authenticate(function() {
-        R.request({
-            method: "currentUser",
-            success: function(response) {
-                var template = '<img src="{{ result.icon }}" />';
-                var icon = $.mustache(template, response);
-                $('#header').append(icon);
+                    if (songkick_artists_map[artist] === undefined) {
+                        songkick_artists_map[artist] = [];
+                    }
+                    songkick_artists_map[artist].push(event[x].id);
+                }
+            }
 
-                var template = '<p>Hey, {{ result.firstName }}!</p>';
-                var greeting = $.mustache(template, response);
-                $('#header').append(greeting);
-            },
-            error: function(response) {
-                alert("error");
-            },
+            get_events(response.resultsPage.page + 1);
         });
+    }
+
+    function rdio_authenticated() {
+        var template = '<img src="{{ icon }}" />';
+        var icon = $.mustache(template, R.currentUser);
+        $('#header').append(icon);
+
+        var template = '<p>Hey, {{ firstName }}!</p>';
+        var greeting = $.mustache(template, R.currentUser);
+        $('#header').append(greeting);
 
         R.request({
             method: "getArtistsInCollection",
@@ -93,5 +81,21 @@ R.ready(function() {
                 alert("error");
             },
         });
+    }
+
+    $(document).ready(function() {
+        get_events(1);
+
+        if ('R' in window) {
+            R.ready(function() {
+                if (R.authenticated()) {
+                    rdio_authenticated();           
+                } else {
+                    // XXX: show some unauthenticated ui
+                }
+            });
+        } else {
+            // XXX: handle no rdio error case
+        }
     });
-});
+ })();
